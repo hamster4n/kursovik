6e9a761607792f0aff1831a0394ba1eed23bb518
CostacR <konstantin.koresh@gmail.com>
2016-05-24 06:50:37 +0300

    Update Discount.java

--- order/Discount.java
+++ order/Discount.java
@@ -3,5 +3,27 @@ package kursovik.order;
 /**
  * Created by Hamster4n.
  */
-public class Discount {
+public class Discount { //discount by summ
+
+    private double price;    // For price
+    private double dis;     // For Discount
+    private double quantity; // For Quantity
+
+    void discount() {
+        if (price <= 2000)
+            dis = price * 0.05;
+        else if (price > 2000 && price <= 5000)
+            dis = price * 0.25;
+        else if (price > 5000 && price <= 10000)
+            dis = price * 0.35;
+        else
+          dis = price * 0.5;
+        amt= price - dis;
+
+  }     
+
+    public static void main(String args[])throws IOException {
+        Discount ob=new Discount();
+        ob.discount();
+    }
 }
