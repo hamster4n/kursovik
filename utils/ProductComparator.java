3481179ca6ddfefe5bfab402e38d5469d7649521
Ihor Solohub <IhorSolohub@i.ua>
2016-05-26 17:25:33 +0300

    Код для порівняння продуктів

--- utils/ProductComparator.java
+++ utils/ProductComparator.java
@@ -1,7 +1,24 @@
 package kursovik.utils;
 
+import kursovik.product.Product;
+
 /**
  * Created by Администратор on 26.05.2016.
  */
-public class ProductComparator {
+public enum  ProductComparator {
+    PRODUCT_ID{
+        public int compare (Product a, Product b){
+            return Integer.compare(a.getProductID(),b.getProductID());
+        }
+    },
+    NAME{
+        public int compare (Product a, Product b){
+            return a.getName().compareTo(b.getName());
+        }
+    },
+    PRICE{
+        public int compare(Product a, Product b){
+            return Integer.compare(a.getPrice(), b.getPrice());
+        }
+    };
 }
