a3d2676d196128d746d586104f6fce887d3b62a6
Ihor Solohub <IhorSolohub@i.ua>
2016-05-26 17:06:51 +0300

    Добавив поля

--- product/Bait.java
+++ product/Bait.java
@@ -3,5 +3,53 @@ package kursovik.product;
 /**
  * Created by Hamster4n.
  */
-public class Bait {
+public class Bait extends Product {
+
+    private String baitType;
+    private String color;
+    private int weight;
+    private static final String category = "Bait";
+
+    public Bait(String baitType, String color, int weight) {
+        this.baitType = baitType;
+        this.color = color;
+        this.weight = weight;
+    }
+
+    public String getBaitType() {
+        return baitType;
+    }
+
+    public void setBaitType(String baitType) {
+        this.baitType = baitType;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public int getWeight() {
+        return weight;
+    }
+
+    public void setWeight(int weight) {
+        this.weight = weight;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    @Override
+    public String toString() {
+        return "Bait{" +
+                "baitType='" + baitType + '\'' +
+                ", color='" + color + '\'' +
+                ", weight=" + weight +
+                '}';
+    }
 }
