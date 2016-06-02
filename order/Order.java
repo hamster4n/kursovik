325db689823d8f78b3dfa02695b1c81e94ca24de
CostacR <konstantin.koresh@gmail.com>
2016-06-02 06:30:22 +0300

    Update Order.java

--- order/Order.java
+++ order/Order.java
@@ -4,4 +4,17 @@ package kursovik.order;
  * Created by Hamster4n.
  */
 public class Order {
+  
+  
+   public int getQuantity() {
+        return quantity;
+   }  
+  
+  public void addQuantity() {
+        quantity++;
+  }
+
+  public void removeQuantity() {
+        quantity--;
+  }
 }
